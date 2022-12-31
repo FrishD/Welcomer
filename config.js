8ceef53b735ae790b04b6e7961ca67c6e22bb754
Shay <97289930+FrishD@users.noreply.github.com>
2022-12-31 15:14:39 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.Token = `OTU4MzYzNTQyNTM1NDI2MDY4.GRyNiH.UuRk8fTZUuy-NAxr7fZwvUOoU1j7vlaSBQU528`; //Bot Token - Important
+exports.Default_Prefix = `K!`; //Bot Default Prefix - Important
+exports.Color = `blue`; //Bot All Embeds Color - Use CAPS For Name - Important
+exports.WelcomeImage = ``; //Welcome Image Link - Default Image By Me (If No Link Provided)
+exports.LeaveImage = ``; //Leave Image Link - Default Image By Me (If No Link Provided)
+exports.Support = `https://discord.gg/Vjzfq9hwtj`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+//Make Sure To On Intents!
